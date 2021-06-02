393a638705b1c56a413ad65a65a07cf9a3adfc84
Paul Kirov <paulkirov@Pauls-MacBook-Pro.local>
2021-06-02 12:52:52 +0300

    insta service implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ErrorHandler } from "./services";
+import { getInstagramMedia, refreshToken } from "./services/instagram";
+import { AppContext } from "./contexts";
+const App = () => {
+  const [instaData, setInstaData] = useState();
+  const [masterClasses, setMasterClasses] = useState();
 
-const App = () =>{
+  const handleInstaMedia = async () => {
+    try {
+      const {
+        data: { data: instaData },
+      } = await getInstagramMedia();
+      const mcs = instaData.filter(({ caption }) =>
+        caption.includes("мк_артстудиянакотовского")
+      );
+      setInstaData(instaData);
+      setMasterClasses(mcs);
+      console.log(instaData, mcs);
+    } catch (error) {
+      ErrorHandler({ moduleName: "App", cameFrom: "handleInstaMedia", error });
+    }
+  };
+
+  useEffect(handleInstaMedia, []);
   return (
-    <Router>
-      <div>
-      Hi! I am your app!
-      </div>
-    </Router>
-  )
-}
+    <AppContext.Provider value={{ instaData, masterClasses }}>
+      <Router>
+        <div>Hi! I am your app!</div>
+      </Router>
+    </AppContext.Provider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+export const ErrorHandler = ({ error, cameFrom, moduleName }) => {
+	console.log(`${moduleName}--->${cameFrom}--->`, String(error));
+};
+
+export default {
+	ErrorHandler,
+};
